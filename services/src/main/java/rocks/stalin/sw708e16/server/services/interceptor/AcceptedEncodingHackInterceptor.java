5eef19cbf089257c96aaccc5b153690330be2959
Jesper Jensen <delusionalLogic@gmail.com>
2016-12-06 14:41:46 +0100

    Added the X-Accept-Encoding header
    Summary:
    I've added a header that overwrite th Accept-Encoding header. Take that
    chrome
    
    Reviewers: VirtualSatai
    
    Reviewed By: VirtualSatai
    
    Subscribers: VirtualSatai
    
    Differential Revision: https://review.slashwin.dk/D59

---
+++ services/src/main/java/rocks/stalin/sw708e16/server/services/interceptor/AcceptedEncodingHackInterceptor.java
@@ -0,0 +1,45 @@
+package rocks.stalin.sw708e16.server.services.interceptor;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Priority;
+import javax.ws.rs.Priorities;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.ext.Provider;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Provider
+@Priority(Priorities.HEADER_DECORATOR)
+@Component
+public class AcceptedEncodingHackInterceptor implements ContainerRequestFilter {
+    Logger logger = LoggerFactory.getLogger(AcceptedEncodingHackInterceptor.class);
+
+    @Override
+    public void filter(ContainerRequestContext requestContext) throws IOException {
+        if(requestContext.getHeaders() != null) {
+            List<String> xAccept = requestContext.getHeaders().get("X-Accept-Encoding");
+            if (xAccept == null)
+                return;
+
+            if(xAccept.size() == 1)
+                return;
+            String xAcceptHeader = xAccept.get(0);
+
+            List<String> accept = requestContext.getHeaders().get("Accept-Encoding");
+            if(accept == null)
+                accept = new ArrayList<>(1);
+
+            logger.info("Dumping the Accept-Encoding header, since the X-Accept-Encoding is specified");
+            if(accept.size() >= 1) {
+                accept.set(0, xAcceptHeader);
+            } else {
+                accept.add(0, xAcceptHeader);
+            }
+        }
+    }
+}
